ca1fef675520ccd063583784b9b43f247367ec48
Ihuarraquax <zablo432432@o2.pl>
2018-11-26 22:46:32 +0100

    Add files via upload

--- Project32_VS2015/Project32_VS2015/AddTwo.asm
+++ Project32_VS2015/Project32_VS2015/AddTwo.asm
@@ -1,15 +1,26 @@
 .386
 .MODEL flat, stdcall
- 
  GetStdHandle PROTO, nStdHandle: DWORD 
  WriteConsoleA PROTO, handle: DWORD, lpBuffer:PTR BYTE, nNumberOfBytesToWrite:DWORD, lpNumberOfBytesWritten:PTR DWORD, lpReserved:DWORD
  ReadConsoleA PROTO :DWORD,:DWORD,:DWORD,:DWORD,:DWORD
  ExitProcess PROTO, dwExitCode: DWORD 
  fillRegister PROTO
+ GetCurrentDirectoryA PROTO :DWORD, :DWORD
  ScanInt PROTO
  atoi PROTO :DWORD
+ lstrcatA PROTO :DWORD, :DWORD
+ CreateFileA PROTO :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD, :DWORD
+ CreateDirectoryA PROTO :DWORD,:DWORD
+
  STD_OUTPUT_HANDLE equ -11
  STD_INPUT_HANDLE equ -10
+	GENERIC_READ equ 80000000h
+	GENERIC_WRITE equ 40000000h
+	CREATE_NEW equ 1
+	CREATE_ALWAYS equ 2
+	OPEN_EXISTING equ 3
+	OPEN_ALWAYS equ 4
+
  .data
  
  outputHandle DD ?
@@ -17,44 +28,32 @@
 
  zmienna1 DD ?
  zmienna2 DD ?
- zmienna3 DD ?
+ zmienna3 DD 6d
  zmienna4 DD ?
 
- wiadomosc Db "Podaj 1 liczbe!", 10d
  iloscLiter dd 16d
  zmiennaZnakow dd ?
-
- wczytanych DD ?
- inBuffer DD 8 dup(?)
+ 
+ bufferSciezki DD 30 dup(?)
+ nazwaFolderu BYTE "\Grupa5",0
+ twojeNazwisko BYTE "\Zablocki.dat", 0
  .code
  main PROC
-	push STD_OUTPUT_HANDLE
-	call GetStdHandle
-	mov outputHandle, EAX
-	push	STD_INPUT_HANDLE
-	call	GetStdHandle	; wywo³anie funkcji GetStdHandle
-	mov	inputHandle, EAX	; deskryptor wejœciowego bufora konsoli
-
-	push 0
-	push offset wczytanych
-	push 10
-	push offset zmienna1
-	push inputHandle
-	call ReadConsoleA
-	mov EBX, offset zmienna1
-	add EBX, wczytanych
-	mov [EBX-2], BYTE PTR 0
-
-	mov EBX, wczytanych-2
-	lejbel:
 	
+	INVOKE GetCurrentDirectoryA,0,0
+	INVOKE GetCurrentDirectoryA,EAX,offset bufferSciezki
 	
+	INVOKE lstrcatA,offset bufferSciezki,offset nazwaFolderu
+	INVOKE CreateDirectoryA,offset bufferSciezki,0
 
-	mov EBX, offset zmienna1
-	call ScanInt
+	INVOKE GetCurrentDirectoryA,0,0
+	INVOKE GetCurrentDirectoryA,EAX,offset bufferSciezki
+
+	INVOKE lstrcatA,offset bufferSciezki,offset twojeNazwisko
+	;
+	INVOKE CreateFileA, offset bufferSciezki,GENERIC_READ OR GENERIC_WRITE,0,0,CREATE_ALWAYS,0,0
+
 	
-
-
 
 	push 0
 	call ExitProcess
